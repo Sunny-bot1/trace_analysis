94026d2a36b9d28e8b2f578373cadae08e2932cc
Sunny-bot1 <592045536@qq.com>
2024-02-27 13:34:47 +0000

    完善w2v向量化过程

--- time_slice/w2v.py
+++ time_slice/w2v.py
@@ -1,23 +1,39 @@
 from gensim.models import Word2Vec
 import pandas as pd
+import os
 
 dimension = 64
 
-data = pd.read_csv('is_feature_merge.csv')
-sentences = data.reset_index(drop=False).apply(lambda row: row.tolist(), axis=1).tolist()
-# print(sentences[0])
+def process_csv(file_path):
 
-model = Word2Vec(sentences, vector_size=dimension, window=5, min_count=1, workers=4)
-# print("word:",model.wv.index_to_key)
+    data = pd.read_csv(file_path, header=None)
+    sentences = data.reset_index(drop=False).apply(lambda row: row.tolist(), axis=1).tolist()
 
-def get_vector(row):
-    vectors = [model.wv[str(num)] for num in row if str(num) in model.wv]
-    if vectors:
-        return sum(vectors) / len(vectors)  
-    else:
-        return [0] * dimension  
+    model = Word2Vec(sentences, vector_size=dimension, window=5, min_count=1, workers=4)
 
-data['feature_vector'] = data.apply(get_vector, axis=1)
-data.to_csv('is_feature_vector.csv', header=True, index=False)
+    def get_vector(row):
+        vectors = [model.wv[str(num)] for num in row if str(num) in model.wv]
+        if vectors:
+            return sum(vectors) / len(vectors)  
+        # else:
+        #     return [0] * dimension  
 
-# print(data['feature_vector'])
+    new_data = pd.DataFrame({'ts': None, 'id': data.iloc[:, 0], 'feature_vector': data.apply(get_vector, axis=1)})
+
+    time_step = 0
+
+    for index, row in new_data.iterrows():
+        if row.isnull().all():
+            time_step += 1
+        else:
+            new_data.at[index, 'ts'] = time_step
+
+    new_data.dropna(axis=0, how='any', inplace=True)
+
+    print(time_step)
+    new_data.to_csv(os.path.join('output/feature_vector', file_name), header=True, index=False)
+
+
+for file_name in os.listdir('data'):
+    if file_name.endswith('.csv'):
+        process_csv(os.path.join('data', file_name))
